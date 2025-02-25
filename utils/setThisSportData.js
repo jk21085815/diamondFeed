76b517b03e58d1e431c4d6264381d5fbd0246d5c
Jignesh142142 <jigneshkumar142@gmail.com>
2025-02-25 17:58:40 +0530

    ok

--- utils/setThisSportData.js
+++ utils/setThisSportData.js
@@ -25,6 +25,7 @@ const setThisSportData = async(eventlist,SportName) => {
                     }
                 })
                 let fetchMarketDatajson = await fetchMarketData.json()
+                console.log(marketId,fetchMarketDatajson,'json dataaaaaaaaaaaaaaaaa')
                 return fetchMarketDatajson.result[0]
             }
             async function fetchBMBook(eventId=27996014) {
