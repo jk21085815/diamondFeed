6ef55744988e8dbad082f4e04d43bd5f2ed4bd34
Jignesh142142 <jigneshkumar142@gmail.com>
2025-02-25 15:59:43 +0530

    ok

--- utils/geteventlist.js
+++ utils/geteventlist.js
@@ -34,7 +34,7 @@ const getEventList = async(sportId,sportName) => {
         return dateToCheck >= fiveDaysAgo && dateToCheck <= currentDate;
     }
     // cron.schedule('00 */6 * * *', async() => {
-    cron.schedule('*/30 * * * *', async() => {
+    cron.schedule('*/30 * * * * *', async() => {
             console.log(`Set ${sportName} CompId Cron Started.....111111111111111111111111111111111111111111111111`)
             try{
                 async function geteventListBySportId () {
