0873be264be6bff6ab921dae915f01a8b115f8ff
Jignesh142142 <jigneshkumar142@gmail.com>
2025-02-26 13:48:23 +0530

    ok

--- utils/setThisSportData.js
+++ utils/setThisSportData.js
@@ -202,11 +202,11 @@ const setThisSportData = async(eventlist,SportName) => {
                         let tempObj = {
                             "marketId": tempObjfancy.id,
                             "marketTime": new Date(),
-                            "bettingType": "BOOKMAKER",
                             "provider": "DIAMOND",
                             "marketName": tempObjfancy.name,
                             "bettingType": "LINE",
                             "marketType": "FANCY",
+                            "status": tempObjfancy.is_active == 1?"OPEN":"CLOSED",
                             "noValue": tempObjfancy.l1,
                             "noRate": tempObjfancy.ls1,
                             "yesValue": tempObjfancy.b1,
