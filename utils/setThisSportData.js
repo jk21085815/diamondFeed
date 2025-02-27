26d037a3e5136d01bc2aa03dfaa52a8d7df0be2c
Jignesh142142 <jigneshkumar142@gmail.com>
2025-02-27 13:04:35 +0530

    ok

--- utils/setThisSportData.js
+++ utils/setThisSportData.js
@@ -199,7 +199,7 @@ const setThisSportData = async(eventlist,SportName) => {
                         let category = ""
                         let tempObjfancy = fancydata[fancyMarketIdArray[b]]
                         tempObjfancy = JSON.parse(tempObjfancy)
-                        console.log(tempObjfancy,"tempObjfancytempObjfancytempObjfancy")
+                        console.log(tempObjfancy,'tempObjfancytempObjfancy')
                         let tempObj = {
                             "marketId": tempObjfancy.id,
                             "marketTime": new Date(),
