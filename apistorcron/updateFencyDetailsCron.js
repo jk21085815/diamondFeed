308ff135797d13e684eaec14aafd4c987060fa85
Jignesh142142 <jigneshkumar142@gmail.com>
2025-02-27 13:58:17 +0530

    ok

--- apistorcron/updateFencyDetailsCron.js
+++ apistorcron/updateFencyDetailsCron.js
@@ -12,7 +12,7 @@ client.on('connect', () => {
 
 
 module.exports = () => {
-    cron.schedule('*/58 * * * *', async() => {
+    cron.schedule('*/59 * * * *', async() => {
         try{
                     let cricketEventIdsLive
                     cricketEventIdsLive = await client.get('crone_CricketliveEventIds_diamond_UPD'); 
