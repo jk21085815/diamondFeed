35f5fbcbd0e36c69cb49f4a6b99775dc94d7a57a
Jignesh142142 <jigneshkumar142@gmail.com>
2025-03-03 11:33:27 +0530

    ok

--- utils/updateBMLiveMarketDetails.js
+++ utils/updateBMLiveMarketDetails.js
@@ -12,26 +12,28 @@ client.on('connect', () => {
 const updateLiveMarketDetails = async(bookmakerdata) => {
     try{
         for(let a = 0; a<bookmakerdata.length; a++){
-            let marketdata = await client.get(`${bookmakerdata[a].bookmaker_id}_diamond`)
-            if(bookmakerdata[a].bookmaker_id == "105449"){
-                console.log(marketdata,'marketdataaaaaaaaaaaaaaa')
-            }
-            if(marketdata){
-                marketdata = JSON.parse(marketdata)
-                marketdata.status = bookmakerdata[a].data.status
-                let bookmakerrunner = JSON.parse(bookmakerdata[a].data.runners)
-                let runnerIds = Object.keys(bookmakerrunner)
-                for(let c = 0;c<runnerIds.length;c++){
-                    let runner = bookmakerrunner[runnerIds[c]]
-                    let thisrunner = marketdata.runners.find(item => item.runnerId == runner.selection_id)
-                    thisrunner.layPrices[0].price = runner.lay_price
-                    thisrunner.layPrices[0].size = runner.lay_volume
-                    thisrunner.backPrices[0].price = runner.back_price
-                    thisrunner.backPrices[0].size = runner.back_volume
+            if(Object.keys(bookmakerdata[a].data).length !== 0){
+                let marketdata = await client.get(`${bookmakerdata[a].bookmaker_id}_diamond`)
+                if(bookmakerdata[a].bookmaker_id == "105449"){
+                    console.log(marketdata,'marketdataaaaaaaaaaaaaaa')
                 }
-                await client.set(`${marketdata.marketId}_diamond`, JSON.stringify(marketdata), 'EX', 24 * 60 * 60);
-                await client.set(`/topic/diamond_bm_update/${marketdata.marketId}`,JSON.stringify(marketdata));
-                Publishclient.publish(`/topic/diamond_bm_update/${marketdata.marketId}`,JSON.stringify(marketdata));
+                if(marketdata){
+                    marketdata = JSON.parse(marketdata)
+                    marketdata.status = bookmakerdata[a].data.status
+                    let bookmakerrunner = JSON.parse(bookmakerdata[a].data.runners)
+                    let runnerIds = Object.keys(bookmakerrunner)
+                    for(let c = 0;c<runnerIds.length;c++){
+                        let runner = bookmakerrunner[runnerIds[c]]
+                        let thisrunner = marketdata.runners.find(item => item.runnerId == runner.selection_id)
+                        thisrunner.layPrices[0].price = runner.lay_price
+                        thisrunner.layPrices[0].size = runner.lay_volume
+                        thisrunner.backPrices[0].price = runner.back_price
+                        thisrunner.backPrices[0].size = runner.back_volume
+                    }
+                    await client.set(`${marketdata.marketId}_diamond`, JSON.stringify(marketdata), 'EX', 24 * 60 * 60);
+                    await client.set(`/topic/diamond_bm_update/${marketdata.marketId}`,JSON.stringify(marketdata));
+                    Publishclient.publish(`/topic/diamond_bm_update/${marketdata.marketId}`,JSON.stringify(marketdata));
+                }
             }
 
         }
