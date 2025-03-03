2467160e39d3b04b0b2ace0c6cd8879d38c4a272
Jignesh142142 <jigneshkumar142@gmail.com>
2025-03-03 11:33:39 +0530

    ok

--- apistorcron/updateBMMarketDetailsCron.js
+++ apistorcron/updateBMMarketDetailsCron.js
@@ -44,7 +44,6 @@ module.exports = () => {
                         await delay(1000 * 10)
                         fetchMarketData = await fetchBMBook(eventIds[i])
                     }
-                    console.log(fetchMarketData,"fetchMarketDatafetchMarketData")
                     updateBMMarketDetails(fetchMarketData)
                 }
             }
