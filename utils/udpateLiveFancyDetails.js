e5194712705e8e6715de62a8dc366233b6c1e293
Jignesh142142 <jigneshkumar142@gmail.com>
2025-03-04 11:23:17 +0530

    ok

--- utils/udpateLiveFancyDetails.js
+++ utils/udpateLiveFancyDetails.js
@@ -20,117 +20,120 @@ const updateFancyDetailsFunc = async (eventId,fencydata) => {
                     if (market) {
                         let marketData = await client.get(`${key}_diamond`);
                         marketData = marketData ? JSON.parse(marketData) : null;
-                        if (marketData && false) {
-                            marketData.status = market.status1;
-                            marketData.inPlay = market.in_play;
-                            marketData.noValue = market.l1;
-                            marketData.noRate = market.ls1;
-                            marketData.yesValue = market.b1;
-                            marketData.yesRate = market.bs1;
-                            let runner1 = marketData.runners[0]
-                            let runner2 = marketData.runners[1]
-                            let runner3 = marketData.runners[2]
-                            runner1.status = market.status1
-                            runner1.layPrices[0].price = market.l1
-                            runner1.layPrices[0].line = market.ls1
-                            runner1.backPrices[0].price = market.b1
-                            runner1.backPrices[0].line = market.bs1
-                            runner2.status = market.status2
-                            runner2.layPrices[1].price = market.l2
-                            runner2.layPrices[1].line = market.ls2
-                            runner2.backPrices[1].price = market.b2
-                            runner2.backPrices[1].line = market.bs2
-                            runner3.status = market.status3
-                            runner3.layPrices[2].price = market.l3
-                            runner3.layPrices[2].line = market.ls3
-                            runner3.backPrices[2].price = market.b3
-                            runner3.backPrices[2].line = market.bs3
-                            await client.set(`${marketData.marketId}_diamond`, JSON.stringify(marketData), 'EX', 24 * 60 * 60);
-                            fancyArr.push(marketData)
-
-                        }else{
-                            let tempRunner = []
-                            let category = ""
-                            let tempObj = {
-                                "marketId": market.id,
-                                "marketTime": new Date(),
-                                "provider": "DIAMOND",
-                                "marketName": market.name,
-                                "bettingType": "LINE",
-                                "marketType": "FANCY",
-                                "status": market.status1,
-                                "noValue": market.l1,
-                                "noRate": market.ls1,
-                                "yesValue": market.b1,
-                                "yesRate": market.bs1,
-                                "inPlay": market.in_play
-                            }
-                            if(["4","10","12"].includes(market.type_code)){
-                                category = "OVERS"
-                            }else if(["42","20","18","34","22","36"].includes(market.type_code)){
-                                category = "BATSMAN"
-                            }else if(market.type_code == 2){
-                                category = "SINGLE_OVER"
-                            }else if(market.type_code == 28){
-                                category = "ODD_EVEN"
-                            }else if(market.type_code == 6){
-                                category = "BALL_BY_BALL"
+                        try{
+                            if (marketData) {
+                                marketData.status = market.status1;
+                                marketData.inPlay = market.in_play;
+                                marketData.noValue = market.l1;
+                                marketData.noRate = market.ls1;
+                                marketData.yesValue = market.b1;
+                                marketData.yesRate = market.bs1;
+                                let runner1 = marketData.runners[0]
+                                let runner2 = marketData.runners[1]
+                                let runner3 = marketData.runners[2]
+                                runner1.status = market.status1
+                                runner1.layPrices[0].price = market.l1
+                                runner1.layPrices[0].line = market.ls1
+                                runner1.backPrices[0].price = market.b1
+                                runner1.backPrices[0].line = market.bs1
+                                runner2.status = market.status2
+                                runner2.layPrices[1].price = market.l2
+                                runner2.layPrices[1].line = market.ls2
+                                runner2.backPrices[1].price = market.b2
+                                runner2.backPrices[1].line = market.bs2
+                                runner3.status = market.status3
+                                runner3.layPrices[2].price = market.l3
+                                runner3.layPrices[2].line = market.ls3
+                                runner3.backPrices[2].price = market.b3
+                                runner3.backPrices[2].line = market.bs3
+                                await client.set(`${marketData.marketId}_diamond`, JSON.stringify(marketData), 'EX', 24 * 60 * 60);
+                                fancyArr.push(marketData)
+    
                             }else{
-                                category = "OTHER"
+                                let tempRunner = []
+                                let category = ""
+                                let tempObj = {
+                                    "marketId": market.id,
+                                    "marketTime": new Date(),
+                                    "provider": "DIAMOND",
+                                    "marketName": market.name,
+                                    "bettingType": "LINE",
+                                    "marketType": "FANCY",
+                                    "status": market.status1,
+                                    "noValue": market.l1,
+                                    "noRate": market.ls1,
+                                    "yesValue": market.b1,
+                                    "yesRate": market.bs1,
+                                    "inPlay": market.in_play
+                                }
+                                if(["4","10","12"].includes(market.type_code)){
+                                    category = "OVERS"
+                                }else if(["42","20","18","34","22","36"].includes(market.type_code)){
+                                    category = "BATSMAN"
+                                }else if(market.type_code == 2){
+                                    category = "SINGLE_OVER"
+                                }else if(market.type_code == 28){
+                                    category = "ODD_EVEN"
+                                }else if(market.type_code == 6){
+                                    category = "BALL_BY_BALL"
+                                }else{
+                                    category = "OTHER"
+                                }
+                                tempObj.category = category
+                                let tempObjrunner1 = 
+                                {
+                                    "status": market.status1,
+                                    "metadata": "",
+                                    "runnerName": market.name,
+                                    "runnerId": market.id + "1",
+                                    "layPrices": [{
+                                        "price":market.l1,
+                                        "line":market.ls1
+                                    }],
+                                    "backPrices": [{
+                                        "price":market.b1,
+                                        "line":market.bs1
+                                    }]
+                                }
+                                let tempObjrunner2 = 
+                                {
+                                    "status": market.status2,
+                                    "metadata": "",
+                                    "runnerName": market.name,
+                                    "runnerId": market.id + "2",
+                                    "layPrices": [{
+                                        "price":market.l2,
+                                        "line":market.ls2
+                                    }],
+                                    "backPrices": [{
+                                        "price":market.b2,
+                                        "line":market.bs2
+                                    }]
+                                }
+                                let tempObjrunner3 = 
+                                {
+                                    "status": market.status3,
+                                    "metadata": "",
+                                    "runnerName": market.name,
+                                    "runnerId": market.id + "3",
+                                    "layPrices": [{
+                                        "price":market.l3,
+                                        "line":market.ls3
+                                    }],
+                                    "backPrices": [{
+                                        "price":market.b3,
+                                        "line":market.bs3
+                                    }]
+                                }
+                                tempRunner.push(tempObjrunner1)
+                                tempRunner.push(tempObjrunner2)
+                                tempRunner.push(tempObjrunner3)
+                                tempObj.runners = tempRunner
+                                await client.set(`${tempObj.marketId}_diamond`, JSON.stringify(tempObj), 'EX', 24 * 60 * 60);
+                                fancyArr.push(tempObj)
                             }
-                            tempObj.category = category
-                            let tempObjrunner1 = 
-                            {
-                                "status": market.status1,
-                                "metadata": "",
-                                "runnerName": market.name,
-                                "runnerId": market.id + "1",
-                                "layPrices": [{
-                                    "price":market.l1,
-                                    "line":market.ls1
-                                }],
-                                "backPrices": [{
-                                    "price":market.b1,
-                                    "line":market.bs1
-                                }]
-                            }
-                            let tempObjrunner2 = 
-                            {
-                                "status": market.status2,
-                                "metadata": "",
-                                "runnerName": market.name,
-                                "runnerId": market.id + "2",
-                                "layPrices": [{
-                                    "price":market.l2,
-                                    "line":market.ls2
-                                }],
-                                "backPrices": [{
-                                    "price":market.b2,
-                                    "line":market.bs2
-                                }]
-                            }
-                            let tempObjrunner3 = 
-                            {
-                                "status": market.status3,
-                                "metadata": "",
-                                "runnerName": market.name,
-                                "runnerId": market.id + "3",
-                                "layPrices": [{
-                                    "price":market.l3,
-                                    "line":market.ls3
-                                }],
-                                "backPrices": [{
-                                    "price":market.b3,
-                                    "line":market.bs3
-                                }]
-                            }
-                            tempRunner.push(tempObjrunner1)
-                            tempRunner.push(tempObjrunner2)
-                            tempRunner.push(tempObjrunner3)
-                            tempObj.runners = tempRunner
-                            await client.set(`${tempObj.marketId}_diamond`, JSON.stringify(tempObj), 'EX', 24 * 60 * 60);
-                            fancyArr.push(tempObj)
-                            
+                        }catch(error){
+                            console.log(error,eventId,":Errorrrrrr")
                         }
                     }
                 }
