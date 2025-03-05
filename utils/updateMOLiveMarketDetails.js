bfd15d98c0249da1962266a757c0ca06b8a632c8
Jignesh142142 <jigneshkumar142@gmail.com>
2025-03-05 11:10:23 +0530

    ok

--- utils/updateMOLiveMarketDetails.js
+++ utils/updateMOLiveMarketDetails.js
@@ -36,7 +36,9 @@ const updateLiveMarketDetails = async(marketIds) => {
                                 runner.layPrices = fetchMarketDatajson[i].runners[j].ex.availableToLay
                                 runner.backPrices = fetchMarketDatajson[i].runners[j].ex.availableToBack
                                 runner.status = fetchMarketDatajson[i].runners[j].status
-                                // console.log(runner,'runnerrrrrrrrrrrrrrr')
+                                if(fetchMarketDatajson[i].marketId == "1.240213061"){
+                                    // console.log(runner,'runnerrrrrrrrrrrrrrr')
+                                }
                             }
                         }
                         await client.set(`${fetchMarketDatajson[i].marketId}_diamond`,JSON.stringify(marketdata),'EX',24 * 60 * 60)
