85c74efc0680e037029b498453c14a2c3d8ddec7
Jignesh142142 <jigneshkumar142@gmail.com>
2025-03-06 12:11:02 +0530

    ok

--- utils/setThisSportData.js
+++ utils/setThisSportData.js
@@ -333,7 +333,7 @@ const setThisSportData = async(eventlist,SportName) => {
                         // }
                         if(["4","10","12"].includes(tempObjfancy.type_code)){
                             category = "OVERS"
-                        }else if(["42","20","18","34","22","36"].includes(tempObjfancy.type_code)){
+                        }else if(["42","20","18","22","36","14"].includes(tempObjfancy.type_code)){
                             category = "BATSMAN"
                         }else if(tempObjfancy.type_code == 2){
                             category = "SINGLE_OVER"
