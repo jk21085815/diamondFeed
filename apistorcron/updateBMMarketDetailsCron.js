be0b8a2b5229f177134090728a504f9bac47dfc0
Jignesh142142 <jigneshkumar142@gmail.com>
2025-03-07 16:45:57 +0530

    ok

--- apistorcron/updateBMMarketDetailsCron.js
+++ apistorcron/updateBMMarketDetailsCron.js
@@ -12,46 +12,49 @@ client.on('connect', () => {
 
 
 module.exports = () => {
-    cron.schedule('* * * * *', async() => {
-        try{
-            let cricketEventIdsLive
-            cricketEventIdsLive = await client.get('crone_CricketliveEventIds_diamond_UPD'); 
-            cricketEventIdsLive = JSON.parse(cricketEventIdsLive)
-            otherEventIdsLive = await client.get('crone_OtherSportLiveEventIds_diamond_UPD'); 
-            otherEventIdsLive = JSON.parse(otherEventIdsLive)
-            let eventIds = cricketEventIdsLive.concat(otherEventIdsLive)
-            // console.log(eventIds.length,'eventIds Idsssssss')
-            function delay(ms) {
-                return new Promise(resolve => setTimeout(resolve, ms));
-            }
-            async function fetchBMBook(eventId) {
-                let fetchMarketData = await fetch(`https://odds.datafeed365.com/api/active-bm/${eventId}`,{
-                    method: 'GET',
-                    headers: {
-                        'Content-type': 'application/json',
-                    }
-                })
-                let fetchMarketDatajson = await fetchMarketData.json()
-                return fetchMarketDatajson.data
-            }
-            if(eventIds){
-                for(let i = 0;i<eventIds.length;i++){
-                    let fetchMarketData
-                    try{
-                        fetchMarketData = await fetchBMBook(eventIds[i])
-                        await delay(1000);
-                    }catch(error){
-                        await delay(1000 * 10)
-                        fetchMarketData = await fetchBMBook(eventIds[i])
-                    }
-                    if(fetchMarketData){
-                        await client.set(`bookmakerlist_${eventIds[i]}`,JSON.stringify(fetchMarketData))
-                        updateBMMarketDetails(fetchMarketData)
+    // cron.schedule('*/0.5 * * * * *', async() => {
+        setInterval(async () => {
+            try{
+                let cricketEventIdsLive
+                cricketEventIdsLive = await client.get('crone_CricketliveEventIds_diamond_UPD'); 
+                cricketEventIdsLive = JSON.parse(cricketEventIdsLive)
+                otherEventIdsLive = await client.get('crone_OtherSportLiveEventIds_diamond_UPD'); 
+                otherEventIdsLive = JSON.parse(otherEventIdsLive)
+                let eventIds = cricketEventIdsLive.concat(otherEventIdsLive)
+                // console.log(eventIds.length,'eventIds Idsssssss')
+                function delay(ms) {
+                    return new Promise(resolve => setTimeout(resolve, ms));
+                }
+                async function fetchBMBook(eventId) {
+                    let fetchMarketData = await fetch(`https://odds.datafeed365.com/api/active-bm/${eventId}`,{
+                        method: 'GET',
+                        headers: {
+                            'Content-type': 'application/json',
+                        }
+                    })
+                    let fetchMarketDatajson = await fetchMarketData.json()
+                    return fetchMarketDatajson.data
+                }
+                if(eventIds){
+                    for(let i = 0;i<eventIds.length;i++){
+                        let fetchMarketData
+                        try{
+                            fetchMarketData = await fetchBMBook(eventIds[i])
+                            await delay(1000);
+                        }catch(error){
+                            await delay(1000 * 10)
+                            fetchMarketData = await fetchBMBook(eventIds[i])
+                        }
+                        if(fetchMarketData){
+                            // await client.set(`bookmakerlist_${eventIds[i]}`,JSON.stringify(fetchMarketData))
+                            updateBMMarketDetails(fetchMarketData)
+                        }
                     }
                 }
+            }catch(error){
+                console.log(error,'Errorrr updateFenctDetailsCrone')
             }
-        }catch(error){
-            console.log(error,'Errorrr updateFenctDetailsCrone')
-        }
-    })
+        }, 505);
+
+    // })
 }
