ef44912516e0f33cf72cb766586de8a4650ffc7c
Jignesh142142 <jigneshkumar142@gmail.com>
2025-03-12 16:14:56 +0530

    ok

--- utils/geteventlist.js
+++ utils/geteventlist.js
@@ -45,7 +45,7 @@ const getEventList = async(sportId,sportName) => {
         return fetchMarketDatajson
     }
     // cron.schedule('00 */6 * * *', async() => {
-    cron.schedule('10 * * * *', async() => {
+    cron.schedule('15 * * * *', async() => {
             console.log(`Set ${sportName} CompId Cron Started.....111111111111111111111111111111111111111111111111`)
             try{
                 async function geteventListBySportId () {
