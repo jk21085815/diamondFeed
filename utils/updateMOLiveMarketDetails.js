83f6035599e7f4a5f24d68e8cebcba9cd49aec71
Jignesh142142 <jigneshkumar142@gmail.com>
2025-04-03 18:38:30 +0530

    ok

--- utils/updateMOLiveMarketDetails.js
+++ utils/updateMOLiveMarketDetails.js
@@ -23,7 +23,7 @@ const updateLiveMarketDetails = async(marketIds) => {
         for(let i = 0;i<fetchMarketDatajson.length;i++){
             if(["OPEN","SUSPENDED","BALL_RUNNING"].includes(fetchMarketDatajson[i].status.trim())){
                 let marketdata = await client.get(`${fetchMarketDatajson[i].marketId}_diamond`)
-                if(fetchMarketDatajson[i].marketId == "1.34173204"){
+                if(fetchMarketDatajson[i].marketId == "1.241627539"){
                     console.log(marketdata,'marketdataaaaaaaaaaa')
                 }
                 if(marketdata){
