29dba1413aca701c95feaeebf078a6311aaf85f3
Jignesh142142 <jigneshkumar142@gmail.com>
2025-04-04 18:32:55 +0530

    ok

--- utils/updateMOLiveMarketDetails.js
+++ utils/updateMOLiveMarketDetails.js
@@ -37,9 +37,9 @@ const updateLiveMarketDetails = async(marketIds) => {
                                 runner.status = fetchMarketDatajson[i].runners[j].status
                             }
                         }
-                        // if(fetchMarketDatajson[i].marketId == "1.241627539"){
-                        //     console.log(marketdata.runners[0].layPrices,'layPriceslayPriceslayPriceslayPriceslayPrices')
-                        // }
+                        if(fetchMarketDatajson[i].marketId == "1.241797085"){
+                            console.log(marketdata.runners[0].layPrices,'layPriceslayPriceslayPriceslayPriceslayPrices')
+                        }
                         await client.set(`${fetchMarketDatajson[i].marketId}_diamond`,JSON.stringify(marketdata),'EX',24 * 60 * 60)
                         await client.set(`/topic/diamond_match_odds_update/${fetchMarketDatajson[i].marketId}`,JSON.stringify(marketdata));
                         Publishclient.publish(`/topic/diamond_match_odds_update/${fetchMarketDatajson[i].marketId}`,JSON.stringify(marketdata));
