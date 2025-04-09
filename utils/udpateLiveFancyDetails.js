8a477b6b379378dc3cd4a030b5dccd7f15229df1
Jignesh142142 <80273820+Jignesh142142@users.noreply.github.com>
2025-04-09 22:46:12 +0530

    ok

--- utils/udpateLiveFancyDetails.js
+++ utils/udpateLiveFancyDetails.js
@@ -14,7 +14,6 @@ const updateFancyDetailsFunc = async (eventId,fencydata) => {
     try {
         let fancyArr = [];
         async function processResponse(response) {
-            let starttime = Date.now()
             for (const key in response) {
                 if (response.hasOwnProperty(key)) {
                     const market = JSON.parse(response[key]);
@@ -29,34 +28,34 @@ const updateFancyDetailsFunc = async (eventId,fencydata) => {
                                 marketData.noRate = market.ls1;
                                 marketData.yesValue = market.b1;
                                 marketData.yesRate = market.bs1;
-                                // let runner1 = marketData.runners[0]
-                                // let runner2 = marketData.runners[1]
-                                // let runner3 = marketData.runners[2]
-                                // if(runner1){
-                                //     runner1.status = market.status1
-                                //     runner1.layPrices[0].price = market.l1
-                                //     runner1.layPrices[0].line = market.ls1
-                                //     runner1.backPrices[0].price = market.b1
-                                //     runner1.backPrices[0].line = market.bs1
-                                // }
-                                // if(runner2){
-                                //     runner2.status = market.status2
-                                //     runner2.layPrices[0].price = market.l2
-                                //     runner2.layPrices[0].line = market.ls2
-                                //     runner2.backPrices[0].price = market.b2
-                                //     runner2.backPrices[0].line = market.bs2
-                                // }else{
-                                //     console.log(marketData.runners,eventId,'marketid with no runnerrrrrr111111')
-                                // }
-                                // if(runner3){
-                                //     runner3.status = market.status3
-                                //     runner3.layPrices[0].price = market.l3
-                                //     runner3.layPrices[0].line = market.ls3
-                                //     runner3.backPrices[0].price = market.b3
-                                //     runner3.backPrices[0].line = market.bs3
-                                // }else{
-                                //     console.log(marketData.runners,eventId,'marketid with no runnerrrr222222222222')
-                                // }
+                                let runner1 = marketData.runners[0]
+                                let runner2 = marketData.runners[1]
+                                let runner3 = marketData.runners[2]
+                                if(runner1){
+                                    runner1.status = market.status1
+                                    runner1.layPrices[0].price = market.l1
+                                    runner1.layPrices[0].line = market.ls1
+                                    runner1.backPrices[0].price = market.b1
+                                    runner1.backPrices[0].line = market.bs1
+                                }
+                                if(runner2){
+                                    runner2.status = market.status2
+                                    runner2.layPrices[0].price = market.l2
+                                    runner2.layPrices[0].line = market.ls2
+                                    runner2.backPrices[0].price = market.b2
+                                    runner2.backPrices[0].line = market.bs2
+                                }else{
+                                    console.log(marketData.runners,eventId,'marketid with no runnerrrrrr111111')
+                                }
+                                if(runner3){
+                                    runner3.status = market.status3
+                                    runner3.layPrices[0].price = market.l3
+                                    runner3.layPrices[0].line = market.ls3
+                                    runner3.backPrices[0].price = market.b3
+                                    runner3.backPrices[0].line = market.bs3
+                                }else{
+                                    console.log(marketData.runners,eventId,'marketid with no runnerrrr222222222222')
+                                }
                                 await client.set(`${marketData.marketId}_diamond`, JSON.stringify(marketData), 'EX', 24 * 60 * 60);
                                 fancyArr.push(marketData)
     
@@ -150,7 +149,6 @@ const updateFancyDetailsFunc = async (eventId,fencydata) => {
                     }
                 }
             }
-            console.log(Date.now()-starttime,'timeeeeeeee');
 
         }
         async function processMarketArray(fencydata) {
