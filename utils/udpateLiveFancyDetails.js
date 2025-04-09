5e9860b263816966f5fb4175d149b69b384a615d
Jignesh142142 <80273820+Jignesh142142@users.noreply.github.com>
2025-04-09 23:11:14 +0530

    ok

--- utils/udpateLiveFancyDetails.js
+++ utils/udpateLiveFancyDetails.js
@@ -21,7 +21,7 @@ const updateFancyDetailsFunc = async (eventId,fencydata) => {
                         let marketData = await client.get(`${key}_diamond`);
                         marketData = marketData ? JSON.parse(marketData) : null;
                         try{
-                            if (marketData) {
+                            if (marketData && false) {
                                 marketData.status = market.status1 == "ACTIVE" ?"OPEN":market.status1,
                                 marketData.inPlay = market.in_play;
                                 marketData.noValue = market.l1;
