2dcc66a29a32409904228b1469b66d597b126ae7
Jignesh142142 <80273820+Jignesh142142@users.noreply.github.com>
2025-05-18 15:13:57 +0530

    ok

--- utils/udpateLiveFancyDetails.js
+++ utils/udpateLiveFancyDetails.js
@@ -189,9 +189,9 @@ const updateFancyDetailsFunc = async (eventId,fencydata) => {
                 }
             });
             await Promise.all(marketPromises);
-            if(eventId == "34316676"){
-                console.log(fancyArr,'fancyArrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr');
-            }
+            // if(eventId == "34316676"){
+            //     console.log(fancyArr,'fancyArrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr');
+            // }
             
 
         }
@@ -211,8 +211,8 @@ const updateFancyDetailsFunc = async (eventId,fencydata) => {
             let eventData = await client.get(`${eventId}_diamondEventData`);
             eventData = JSON.parse(eventData);
             eventData.markets.fancyMarkets = fancyArr;
-            if(eventId == "34296856"){
-                // console.log(fancyArr,'fancyArrrrrrrrr')
+            if(eventId == "34321472"){
+                console.log(fancyArr,'fancyArrrrrrrrr')
             }
             await client.set(`${eventId}_diamondEventData`, JSON.stringify(eventData), 'EX', 24 * 60 * 60);
             const api1ResponseTime = Date.now() - startTime;
