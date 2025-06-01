de68acab0f2c5b1d290743078deebd88b86c1a6f
Jignesh142142 <80273820+Jignesh142142@users.noreply.github.com>
2025-06-01 18:58:28 +0530

    ok

--- utils/updateMOLiveMarketDetails2.js
+++ utils/updateMOLiveMarketDetails2.js
@@ -1,10 +1,7 @@
 const redis = require('redis');
 const client = redis.createClient({url:process.env.redisurl});
-const clientme = redis.createClient({url:process.env.redisurlme});
-const Publishclient = redis.createClient({url:process.env.redisurl});
+const updateMoDetail3 = require('./updateMOLiveMarketDetails3')
 client.connect()
-clientme.connect()
-Publishclient.connect()
 client.on('error', (err) => {
     console.log(`Error(In setMarketIdsCron.js):${err}`);
 });
@@ -17,35 +14,8 @@ const updateLiveMarketDetails2 = async(bookdata,i) => {
         console.log(i,'iiiiii222222222222')
         if(["OPEN","SUSPENDED","BALL_RUNNING"].includes(bookdata.status.trim())){
             let marketdata = await client.get(`${bookdata.marketId}_diamond`)
-            console.log(i,'iiiiii333333333333')
-
             if(marketdata){
-                try{
-                    marketdata = JSON.parse(marketdata)
-                    marketdata.status = bookdata.status
-                    for(let j = 0;j<bookdata.runners.length;j++){
-                        runner = marketdata.runners.find(item => (item && item.runnerId && item.runnerId == bookdata.runners[j].selectionId))
-                        if(runner){
-                            runner.layPrices = bookdata.runners[j].ex.availableToLay
-                            runner.backPrices = bookdata.runners[j].ex.availableToBack
-                            // if(bookdata.marketId == "1.244295255"){
-                            //     console.log(runner.layPrices, 'runner.layPrices');
-                                
-                            // }
-                            runner.status = bookdata.runners[j].status
-                        }
-                    }
-                    if(bookdata.marketId == "1.244359309"){
-                        console.log(marketdata.runners[0].backPrices,'backpriceeeeee')
-                    }
-                    client.set(`${bookdata.marketId}_diamond`,JSON.stringify(marketdata),'EX',24 * 60 * 60)
-                    clientme.set(`${bookdata.marketId}_diamond`,JSON.stringify(marketdata),'EX',24 * 60 * 60)
-                    client.set(`/topic/diamond_match_odds_update/${bookdata.marketId}`,JSON.stringify(marketdata));
-                    Publishclient.publish(`/topic/diamond_match_odds_update/${bookdata.marketId}`,JSON.stringify(marketdata));
-                }catch(error){
-                    console.log(error,'Errorrrrrrrrrrrr')
-                }
-                
+                updateMoDetail3(marketdata)
             }
         }
         
