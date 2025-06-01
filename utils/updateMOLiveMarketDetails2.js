978b8a8837e9980fd71b4331c9cc54641255652b
Jignesh142142 <80273820+Jignesh142142@users.noreply.github.com>
2025-06-01 19:13:16 +0530

    ok

--- utils/updateMOLiveMarketDetails2.js
+++ utils/updateMOLiveMarketDetails2.js
@@ -1,21 +1,48 @@
 const redis = require('redis');
 const client = redis.createClient({url:process.env.redisurl});
-const updateMoDetail3 = require('./updateMOLiveMarketDetails3')
+const clientme = redis.createClient({url:process.env.redisurlme});
+const Publishclient = redis.createClient({url:process.env.redisurl});
 client.connect()
+clientme.connect()
+Publishclient.connect()
 client.on('error', (err) => {
     console.log(`Error(In setMarketIdsCron.js):${err}`);
 });
 client.on('connect', () => {
     // console.log('Connected to Redis1');
 });
-const updateLiveMarketDetails2 = async(bookdata,i) => {
+const updateLiveMarketDetails2 = async(bookdata) => {
     let runner
     try{
-        // console.log(i,'iiiiii222222222222')
         if(["OPEN","SUSPENDED","BALL_RUNNING"].includes(bookdata.status.trim())){
             let marketdata = await client.get(`${bookdata.marketId}_diamond`)
             if(marketdata){
-                updateMoDetail3(bookdata,marketdata,i)
+                try{
+                    marketdata = JSON.parse(marketdata)
+                    marketdata.status = bookdata.status
+                    for(let j = 0;j<bookdata.runners.length;j++){
+                        runner = marketdata.runners.find(item => (item && item.runnerId && item.runnerId == bookdata.runners[j].selectionId))
+                        if(runner){
+                            runner.layPrices = bookdata.runners[j].ex.availableToLay
+                            runner.backPrices = bookdata.runners[j].ex.availableToBack
+                            // if(bookdata.marketId == "1.244295255"){
+                            //     console.log(runner.layPrices, 'runner.layPrices');
+                                
+                            // }
+                            runner.status = bookdata.runners[j].status
+                        }
+                    }
+                    if(bookdata.marketId == "1.244359309"){
+                        console.log(marketdata.runners[0].backPrices,'backpriceeeeee')
+                    }
+                    client.set(`${bookdata.marketId}_diamond`,JSON.stringify(marketdata),'EX',24 * 60 * 60)
+                    clientme.set(`${bookdata.marketId}_diamond`,JSON.stringify(marketdata),'EX',24 * 60 * 60)
+                    client.set(`/topic/diamond_match_odds_update/${bookdata.marketId}`,JSON.stringify(marketdata));
+                    Publishclient.publish(`/topic/diamond_match_odds_update/${bookdata.marketId}`,JSON.stringify(marketdata));
+                }catch(error){
+                    console.log(error,'Errorrrrrrrrrrrr')
+                }
+                
             }
         }
         
