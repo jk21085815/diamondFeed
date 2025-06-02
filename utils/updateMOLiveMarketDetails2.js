446a9bfb2149efd8865eb4acb35a99509d877461
Jignesh142142 <jigneshkumar142@gmail.com>
2025-06-02 16:18:12 +0530

    ok

--- utils/updateMOLiveMarketDetails2.js
+++ utils/updateMOLiveMarketDetails2.js
@@ -50,7 +50,7 @@ const updateLiveMarketDetails2 = async(bookdataArray) => {
 
                     if(bookdata.marketId == "1.244277489"){
                         // console.log(marketdata.runners[0].backPrices,'backpriceeeeee22222222')
-                        log(`Logs 2 [${timestamp}] ${JSON.stringify(marketdata.runners[0].backPrices)}\n`);
+                        // log(`Logs 2 [${timestamp}] ${JSON.stringify(marketdata.runners[0].backPrices)}\n`);
                     }
 
                     const updatedStr = JSON.stringify(marketdata);
