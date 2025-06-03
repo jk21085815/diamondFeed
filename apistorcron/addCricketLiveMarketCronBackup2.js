76e5f516cf086aa84d897ce2dca8cdbf7a6d2380
Jignesh142142 <jigneshkumar142@gmail.com>
2025-06-03 11:36:17 +0530

    ok

--- apistorcron/addCricketLiveMarketCronBackup2.js
+++ apistorcron/addCricketLiveMarketCronBackup2.js
@@ -164,7 +164,7 @@ client.on('connect', () => {
                             let thatMO = liveMatchCheckMarket
                             if(thatMO){
                                 const timestamp = new Date().toISOString();
-                                logStream.write(`[${timestamp}]  ${eventData.eventId} ${eventData.status}\n`);
+                                logStream.write(`[${timestamp}]  ${eventData.eventId} ${thatMO.marketId} ${thatMO.status}\n`);
                                 if(['OPEN','SUSPENDED',"BALL_RUNNING"].includes(thatMO.status)){
                                     pushstatus = true
                                 }
