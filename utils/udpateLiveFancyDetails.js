dcedff6194671b1351fbc251ab74cf938b5c443a
Jignesh142142 <jigneshkumar142@gmail.com>
2025-06-18 17:12:51 +0530

    ok

--- utils/udpateLiveFancyDetails.js
+++ utils/udpateLiveFancyDetails.js
@@ -48,7 +48,7 @@ const updateFancyDetailsFunc = async (eventId) => {
                 }
                 console.error('Error fetching data:', error);
             }
-            console.log(Date.now()-startForAPi,eventId, 'APICALLTIME');
+            // console.log(Date.now()-startForAPi,eventId, 'APICALLTIME');
             
             return result;
         }
@@ -221,11 +221,11 @@ const updateFancyDetailsFunc = async (eventId) => {
             eventData = JSON.parse(eventData);
             eventData.markets.fancyMarkets = fancyArr;
             if(eventId == "34423812"){
-                console.log(fancyArr[0]?.marketName,fancyArr[0]?.yesValue,fancyArr[0]?.status)
+                console.log(new Date(),fancyArr[0]?.marketName,fancyArr[0]?.yesValue,fancyArr[0]?.status)
             }
             await client.set(`${eventId}_diamondEventData`, JSON.stringify(eventData), 'EX', 24 * 60 * 60); // event no data redis mathi get krine fancy udpate krine pacho redis ma save krie chie
             await clientme.set(`${eventId}_diamondEventData`, JSON.stringify(eventData), 'EX', 24 * 60 * 60);
-            console.log(Date.now()-startDate,eventId, 'timeTaken');
+            // console.log(Date.now()-startDate,eventId, 'timeTaken');
             
         });        
     } catch (error) {
