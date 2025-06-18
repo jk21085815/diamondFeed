c2f03b2f730bbc96d46e152da43696addb926af5
Jignesh142142 <jigneshkumar142@gmail.com>
2025-06-18 16:26:47 +0530

    ok

--- utils/udpateLiveFancyDetails.js
+++ utils/udpateLiveFancyDetails.js
@@ -221,7 +221,7 @@ const updateFancyDetailsFunc = async (eventId) => {
             eventData = JSON.parse(eventData);
             eventData.markets.fancyMarkets = fancyArr;
             if(eventId == "34429696"){
-                console.log(fancyArr[1]?.marketName,fancyArr[1]?.yesValue,fancyArr[1]?.status,'fancyArrrrrrrrr')
+                console.log(fancyArr[0]?.marketName,fancyArr[0]?.yesValue,fancyArr[0]?.status)
             }
             await client.set(`${eventId}_diamondEventData`, JSON.stringify(eventData), 'EX', 24 * 60 * 60); // event no data redis mathi get krine fancy udpate krine pacho redis ma save krie chie
             await clientme.set(`${eventId}_diamondEventData`, JSON.stringify(eventData), 'EX', 24 * 60 * 60);
