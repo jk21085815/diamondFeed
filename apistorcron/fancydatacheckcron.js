d3bf996cde5f257fa3c13ebdc92290ab5c81c9f2
Jignesh142142 <jigneshkumar142@gmail.com>
2025-06-19 15:14:32 +0530

    ok

--- apistorcron/fancydatacheckcron.js
+++ apistorcron/fancydatacheckcron.js
@@ -8,21 +8,21 @@ module.exports = () => {
     cron.schedule('*/1 * * * *', async() => {
         try{
             // Check file size and rename
-            fs.stat('fancyArray.txt', (err, stats) => {
-            if (err) {
-                console.error('Error getting file stats:', err);
-                return;
-            }
-
-            console.log(`File size: ${stats.size} bytes`);
-            
-            fs.rename('fancyArray.txt', `fancyArray${i}.txt`, (renameErr) => {
-                if (renameErr) {
-                console.error('Error renaming file:', renameErr);
-                return;
+            fs.stat('../utils/fancyArray.txt', (err, stats) => {
+                if (err) {
+                    console.error('Error getting file stats:', err);
+                    return;
                 }
-                console.log('File renamed successfully');
-            });
+
+                console.log(`File size: ${stats.size} bytes`);
+                
+                fs.rename('../utils/fancyArray.txt', `../utils/fancyArray${i}.txt`, (renameErr) => {
+                    if (renameErr) {
+                    console.error('Error renaming file:', renameErr);
+                    return;
+                    }
+                    console.log('File renamed successfully');
+                });
             });
         }catch(error){
             console.log(error,'Errorrr exchagnePageCron')
