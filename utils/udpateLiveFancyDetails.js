811cbd57af0bbaddf0f7cf293f55c9763c7d602b
Jignesh142142 <jigneshkumar142@gmail.com>
2025-06-19 15:37:05 +0530

    ok

--- utils/udpateLiveFancyDetails.js
+++ utils/udpateLiveFancyDetails.js
@@ -220,7 +220,7 @@ const updateFancyDetailsFunc = async (eventId) => {
             // console.log('API Responses:', fancyArr);
             if(fancyArr.length !== 0){
                 logStream.write(`[${new Date()}] ${' eventId: ' + eventId + ' '}  ${JSON.stringify(fancyArr)}\n`);
-                console.log(new Date(Date.now() + (5.5 * 60 *60 *1000)),fancyArr[0]?.marketName,fancyArr[0]?.yesValue,fancyArr[0]?.status,eventId)
+                // console.log(new Date(Date.now() + (5.5 * 60 *60 *1000)),fancyArr[0]?.marketName,fancyArr[0]?.yesValue,fancyArr[0]?.status,eventId)
             }
             await client.set(`/topic/diamond_fancy_update/${eventId}`, JSON.stringify(fancyArr), 'EX', 24 * 60 * 60);  // fancyarray ne event id parthi redis ma save and publish krie chie
             await Publishclient.publish(`/topic/diamond_fancy_update/${eventId}`, JSON.stringify(fancyArr));
