cac47f255f1ae202026e9d20ac46b2c8d9cd62b8
Jignesh142142 <jigneshkumar142@gmail.com>
2025-06-19 15:18:54 +0530

    ok

--- apistorcron/fancydatacheckcron.js
+++ apistorcron/fancydatacheckcron.js
@@ -5,25 +5,28 @@ const path = require('path')
 
 module.exports = () => {
     let i = 1
+    const filePath = path.join(__dirname, '../utils', 'input.txt');
+    console.log(filePath,'filepath')
     cron.schedule('*/1 * * * *', async() => {
         try{
             // Check file size and rename
-            fs.stat('../utils/fancyArray.txt', (err, stats) => {
-                if (err) {
-                    console.error('Error getting file stats:', err);
-                    return;
-                }
 
-                console.log(`File size: ${stats.size} bytes`);
+            // fs.stat('../utils/fancyArray.txt', (err, stats) => {
+            //     if (err) {
+            //         console.error('Error getting file stats:', err);
+            //         return;
+            //     }
+
+            //     console.log(`File size: ${stats.size} bytes`);
                 
-                fs.rename('../utils/fancyArray.txt', `../utils/fancyArray${i}.txt`, (renameErr) => {
-                    if (renameErr) {
-                    console.error('Error renaming file:', renameErr);
-                    return;
-                    }
-                    console.log('File renamed successfully');
-                });
-            });
+            //     fs.rename('../utils/fancyArray.txt', `../utils/fancyArray${i}.txt`, (renameErr) => {
+            //         if (renameErr) {
+            //         console.error('Error renaming file:', renameErr);
+            //         return;
+            //         }
+            //         console.log('File renamed successfully');
+            //     });
+            // });
         }catch(error){
             console.log(error,'Errorrr exchagnePageCron')
         }
