7c3f3042b83bfca2085abc89b1b6ff859d1d977e
Jignesh142142 <jigneshkumar142@gmail.com>
2025-06-19 14:18:46 +0530

    ok

--- utils/udpateLiveFancyDetails.js
+++ utils/udpateLiveFancyDetails.js
@@ -218,15 +218,16 @@ const updateFancyDetailsFunc = async (eventId) => {
         await processMarketArray();
         try{
             // console.log('API Responses:', responses);
+            if(fancyArr.length !== 0){
+                logStream.write(`[${new Date()}] ${' ' + eventId + ' '}  ${JSON.stringify(fancyArr)}\n`);
+                console.log(new Date(Date.now() + (5.5 * 60 *60 *1000)),fancyArr[0]?.marketName,fancyArr[0]?.yesValue,fancyArr[0]?.status,eventId)
+            }
             await client.set(`/topic/diamond_fancy_update/${eventId}`, JSON.stringify(fancyArr), 'EX', 24 * 60 * 60);  // fancyarray ne event id parthi redis ma save and publish krie chie
             await Publishclient.publish(`/topic/diamond_fancy_update/${eventId}`, JSON.stringify(fancyArr));
             let eventData = await client.get(`${eventId}_diamondEventData`);
             eventData = JSON.parse(eventData);
             eventData.markets.fancyMarkets = fancyArr;
-            if(fancyArr.length !== 0){
-                logStream.write(`[${new Date()}] ${' ' + eventId + ' '}  ${JSON.stringify(fancyArr)}\n`);
-                console.log(new Date(Date.now() + (5.5 * 60 *60 *1000)),fancyArr[0]?.marketName,fancyArr[0]?.yesValue,fancyArr[0]?.status,eventId)
-            }
+          
             await client.set(`${eventId}_diamondEventData`, JSON.stringify(eventData), 'EX', 24 * 60 * 60); // event no data redis mathi get krine fancy udpate krine pacho redis ma save krie chie
             await clientme.set(`${eventId}_diamondEventData`, JSON.stringify(eventData), 'EX', 24 * 60 * 60);
             // console.log(Date.now()-startDate,eventId, 'timeTaken');
