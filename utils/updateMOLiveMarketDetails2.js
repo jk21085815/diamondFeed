197d0fd6870bf5e95cfad2e971341b7f956b1ac1
Jignesh142142 <jigneshkumar142@gmail.com>
2025-07-03 15:06:49 +0530

    ok

--- utils/updateMOLiveMarketDetails2.js
+++ utils/updateMOLiveMarketDetails2.js
@@ -47,10 +47,10 @@ const updateLiveMarketDetails2 = async(bookdataArray) => {
                         }
                     }
 
-                    if(bookdata.marketId == "1.245260641"){
-                        console.log(marketdata.runners[0].backPrices,'backpriceeeeee22222222')
+                    // if(bookdata.marketId == "1.245260641"){
+                        // console.log(marketdata.runners[0].backPrices,'backpriceeeeee22222222')
                         // log(`Logs 2 [${timestamp}] ${JSON.stringify(marketdata.runners[0].backPrices)}\n`);
-                    }
+                    // }
 
                     const updatedStr = JSON.stringify(marketdata);
                     const redisKey = `${bookdata.marketId}_diamond`;
